1d4eb4083ba523372d74b30d520dc9a18c84be57
Avish Jain <avishjain0@gmail.com>
2022-07-30 21:49:01 +0530

    card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { Heading, HStack, Select } from "@chakra-ui/react";
+import Card from "./Card";
+
 function App() {
   return (
     <div className="App">
+      <Heading>Seacrh properties to rent</Heading>
+      <HStack bg="white">
+        <Select w="container.xs" placeholder="Select State">
+          <option value="MD">MD</option>
+          <option value="AR">AR</option>
+          <option value="FL">FL</option>
+          <option value="MS">MS</option>
+          <option value="IN">IN</option>
+        </Select>
+        <Select w="container.xs" placeholder="Price">
+          <option value="MD">MD</option>
+          <option value="AR">AR</option>
+          <option value="FL">FL</option>
+          <option value="MS">MS</option>
+          <option value="IN">IN</option>
+        </Select>
+      </HStack>
+      <Card />
     </div>
   );
 }
